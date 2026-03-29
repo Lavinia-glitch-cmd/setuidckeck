f94a28172c75e7619090a45ad8279314efe5fce7
Cartsea Lavinia <laviniac7355@gmail.com>
2026-03-29 18:03:14 +0300

    modified script for all flags

--- date_setuid.sh
+++ date_setuid.sh
@@ -2,9 +2,8 @@
 fisier="/usr/bin/sudo"
 flags=("-A" "-B" "-b" "-C" "-D" "-E" "-e" "-g" "-H" "-h" "-i" "-K" "-k" "-l" "-N" "-n" "-P" "-p" "-R" "-r" "-S"  "-s" "-t" "-U" "-T" "-u" "-V" "-v")
 nume=$(basename "$fisier")
-#i=0;
-#for flag in "${flags[@]}"; do
-	#i=$((i+1))
-#	sudo strace -c "$fisier" "$flag" > /dev/null 2>> "${nume}_date.txt"
-#done
-sudo strace -c "$fisier" -A > /dev/null 2>> "${nume}_date.txt"
+for flag in "${flags[@]}"; do
+	f=${flag#-}
+	output="${nume}_${f}"
+	sudo strace -c "$fisier" "$flag" > /dev/null 2> "$output"
+done
