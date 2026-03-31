de4111aefb9a417ad63fd961a2cab4cb749e9894
Cartsea Lavinia <laviniac7355@gmail.com>
2026-03-31 11:35:36 +0300

    removed interactive -s flag

--- date_setuid.sh
+++ date_setuid.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 fisier="/usr/bin/sudo"
-flags=("-A" "-B" "-b" "-C" "-D" "-E" "-e" "-g" "-H" "-h" "-i" "-K" "-k" "-l" "-N" "-n" "-P" "-p" "-R" "-r" "-S"  "-s" "-t" "-U" "-T" "-u" "-V" "-v")
+flags=("-A" "-B" "-b" "-C" "-D" "-E" "-e" "-g" "-H" "-h" "-i" "-K" "-k" "-l" "-N" "-n" "-P" "-p" "-R" "-r" "-S" "-t" "-U" "-T" "-u" "-V" "-v")
 nume=$(basename "$fisier")
 for flag in "${flags[@]}"; do
 	f=${flag#-}
