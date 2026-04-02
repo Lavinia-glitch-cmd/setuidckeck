defd851869c6ad7c58e40cef0de0ad5edcfbf7b6
Lavinia-glitch-cmd <laviniac7355@gmail.com>
2026-04-02 21:21:18 +0300

    Delete date_setuid.sh

--- date_setuid.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-fisier="/usr/bin/sudo"
-flags=("-A" "-B" "-b" "-C" "-D" "-E" "-e" "-g" "-H" "-h" "-i" "-K" "-k" "-l" "-N" "-n" "-P" "-p" "-R" "-r" "-S" "-t" "-U" "-T" "-u" "-V" "-v")
-nume=$(basename "$fisier")
-for flag in "${flags[@]}"; do
-	f=${flag#-}
-	output="${nume}_${f}"
-	sudo strace -c "$fisier" "$flag" > /dev/null 2> "$output"
-done
