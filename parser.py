8fa63e1075214eae7b065576e621683ef837754e
Cartsea Lavinia <laviniac7355@gmail.com>
2026-04-05 00:15:16 +0300

    uploaded file

---
+++ parser.py
@@ -0,0 +1,29 @@
+import json
+from pathlib import Path
+from structures import Dictionary
+
+def Get_SysCallTable():
+    base_path=Path(__file__).resolve().parent
+    file_path=base_path/"syscall_no.txt"
+    
+    SysCallTable=Dictionary()
+    try:
+        with open(file_path) as f:
+            for line in f:
+                line=line.strip().split()
+                try:
+                    syscall_number=int(line[0])
+                    syscall_name=line[2]
+                    entry_point=line[3] if len(line) > 3 else "N/A"
+                    SysCallTable[syscall_name]=[syscall_number, entry_point]
+                except ValueError: 
+                    continue
+        return SysCallTable
+    except FileNotFoundError:
+        return None
+if __name__ == "__main__":
+    table=Get_SysCallTable()
+    if table:
+        print(json.dumps(table.dict, indent=4))
+    
+        
