56bb95ebf0d2180e9fba2af0e651a2e1d57254a8
Cartsea Lavinia <laviniac7355@gmail.com>
2026-04-12 00:45:15 +0300

    modified names

--- main.py
+++ main.py
@@ -4,30 +4,7 @@ import pandas as pd
 from pathlib import Path
 from strace import GetStraceDictionary
 from parser import Get_SysCallTable
-
-def vectorise(dictionary):
-    vectorised_data={}
-    columns = ["syscall_no", "%time", "seconds", "usecs/call", "calls", "errors"]
-    for file_name, syscall_dict in dictionary.items():
-        rows_file=[]
-        for syscall_name, data_list in syscall_dict.items():
-            rows_data=data_list[0]
-            print(data_list[0])
-            row=[]
-
-            for col in columns:
-                val=rows_data.get(col, 0)
-                try:
-                    row.append(float(val))
-                except (ValueError, TypeError):
-                    row.append(0.0)
-            rows_file.append(row)
-        if rows_file:
-            vectorised_data[file_name] = np.array(rows_file)
-        else:
-            vectorised_data[file_name] = np.zeros((0, len(columns)))
-            
-    return vectorised_data
+from vectorise import GetMatrices
     
 def main():
     base_path = Path(__file__).resolve().parent
@@ -45,7 +22,10 @@ def main():
             if strace_syscall_name in syscalls.dict:
                 syscall_no=syscalls.dict[strace_syscall_name][0][0]
                 strace_dict[strace_syscall_name][0]["syscall_no"] = syscall_no
-    matrices=vectorise(strace_results)
+    matrices=GetMatrices(strace_results, syscalls)
+    np.set_printoptions(threshold=np.inf, suppress=True, linewidth=150)
+
+    #print("\n--- MATRICEA COMPLETĂ PENTRU sudo_A ---")
     print(matrices)
   
 if __name__ == "__main__":
