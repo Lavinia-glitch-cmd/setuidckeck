aeabc8774b931d3c8772b3fafb874d3d5501ff7f
Cartsea Lavinia <laviniac7355@gmail.com>
2026-04-12 01:15:13 +0300

    flattening matrices into vector

---
+++ vectorise.py
@@ -0,0 +1,36 @@
+import numpy as np
+from sklearn.ensemble import IsolationForest
+from sklearn.preprocessing import StandardScaler
+
+def vectorise(matrices):
+    vector=[]
+    file_names=[]
+    for name, mat in matrices.items():
+        line=mat[:, 1:]
+        vector.append(line.flatten())
+        file_names.append(name)
+    return np.array(vector), file_names
+
+def GetMatrices(strace_results, syscalls_dict):
+    matrices={}
+    columns = ["%time", "seconds", "usecs/call", "calls", "errors"]
+    syscalls=[]
+    for syscall_name, entry  in syscalls_dict.items():
+        syscall_no=entry[0][0]
+        syscalls.append((syscall_no, syscall_name))
+    syscalls.sort()
+    for file_name, file_data in strace_results.items():
+        file_matrix = np.zeros((len(syscalls), 6))
+        for i, (sys_no, sys_name) in enumerate(syscalls):
+            file_matrix[i, 0] = sys_no
+            if sys_name in file_data:
+                stats = file_data[sys_name][0]
+                for col_idx, col_name in enumerate(columns, start=1):
+                    val = stats.get(col_name, 0)
+                    try:
+                        file_matrix[i, col_idx] = float(val)
+                    except:
+                        file_matrix[i, col_idx] = 0.0
+        matrices[file_name] = file_matrix
+        
+    return matrices
