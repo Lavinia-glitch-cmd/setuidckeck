c607b435519017d570ba559086d6a1eaeacdce56
Cartsea Lavinia <laviniac7355@gmail.com>
2026-04-12 12:38:11 +0300

    modified file to work for usual strace output

--- strace.py
+++ strace.py
@@ -3,23 +3,36 @@ from structures import Dictionary
 def GetStraceDictionary(file):
     result=Dictionary()
     coloumn_header=[]
+    started_strace=False
+    
     with open(file) as f:
         for line in f:
             line=line.strip()
-            if any(x in line for x in [ "---", "usage", "[", "/usr", "total" ]):
-                continue
-            line=line.split()
-            if line[0] == '%':
+            if not line : continue
+            
+            if "% time" in line and not coloumn_header:
+                line=line.split()
                 time_percentage="".join(line[0:2])
                 coloumn_header=[ time_percentage ] + line[2:]
                 continue
-            if ',' in line[0]:
-                data={}
-                line[0]=float(line[0].replace(',', '.'))
-                if len(line) == 5 :
-                    line.insert(4, '0')
-                data={ coloumn_header[i]: line[i] for i in range(len(line)) }
-                result[line[-1]]=data
+                
+            if "------" in line:
+                if not started_strace:
+                    started_strace=True
+                    continue
+                else:
+                    break
+            if started_strace:
+                line=line.split()
+                
+                if line[0][0].isdigit():
+                    data={}
+                    line[0]=float(line[0].replace(',', '.'))
+                    if len(line) == 5 :
+                        line.insert(4, '0')
+                    data={ coloumn_header[i]: line[i] for i in range(len(line)) }
+                    result[line[-1]]=data
+    print(result.dict)
     return result.dict
                 
 
