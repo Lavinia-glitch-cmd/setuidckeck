b5d4ddf6afe3b8671b0a44e7cfcdab88cd05f6a3
Carstea Lavinia <laviniac7355@gmail.com>
2026-04-18 21:23:02 +0300

    uploaded file for detection

--- detect.py
+++ detect.py
@@ -4,11 +4,20 @@ import numpy as np
 from flags import GenerateVectorForDetection
 from parser import Get_SysCallTable
 
-syscalls=Get_SysCallTable()
-inference=rt.InferenceSession("isolation_forest_onnx")
-_scaler=joblib.load("scaler.pkl")
 
 def detect(file):
+    syscalls=Get_SysCallTable()
+    sess=rt.InferenceSession("isolation_forest_onnx", providers=rt.get_available_providers())
+    _scaler=joblib.load("scaler.pkl")
+    
+    
+   
     
     vector=GenerateVectorForDetection(file, syscalls)
     vector_scaled = _scaler.transform([vector]).astype(np.float32)
+    
+    ###https://onnxruntime.ai/docs/api/python/tutorial.html
+    input_name = sess.get_inputs()[0].name
+    label_name = sess.get_outputs()[0].name
+    
+    pred_onx = sess.run([label_name], {input_name: vector_scaled})[0]
