7ee0a09bcbb1d187c61d92744da3bef6a4df7dc1
Carstea Lavinia <laviniac7355@gmail.com>
2026-04-19 09:40:06 +0300

    updated files to have different models for each binary

--- training.py
+++ training.py
@@ -1,4 +1,5 @@
 import joblib
+from pathlib import Path
 
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import IsolationForest
@@ -13,26 +14,25 @@ from vectorise import GetVectors
 
 def training():
     
-    X=GetVectors()
-    scaler=StandardScaler()
-    X_scaled=scaler.fit_transform(X)
+    all_vectors=GetVectors()
+    Path("models").mkdir(parents=True, exist_ok=True)
+    for binary_name, X in all_vectors.items():
+        scaler=StandardScaler()
+        X_scaled=scaler.fit_transform(X)
+        
+        clf=IsolationForest(contamination=0.01, random_state=42).fit(X_scaled)
     
-    clf=IsolationForest(contamination=0.01, random_state=42).fit(X_scaled)
-    import sys 
-    np.set_printoptions(threshold=sys.maxsize)
-    print(clf.predict(X_scaled))
-
-    joblib.dump(scaler, "scaler.pkl")
-    _type=[('float_input', FloatTensorType([None, X.shape[1]]))]
+        joblib.dump(scaler, f"models/scaler_{binary_name}.pkl")
+        initial_type=[('float_input', FloatTensorType([None, X.shape[1]]))]
     
     
     
     #specified version because of runtime errors: RuntimeError: The model is using version 4 of domain 'ai.onnx.ml' not supported yet by this library. You need to specify target_opset={'ai.onnx.ml': 3}, and '':15 is general
 
-    onx = convert_sklearn(clf, initial_types=_type, target_opset={'':15, 'ai.onnx.ml':3})
+        onx = convert_sklearn(clf, initial_types=initial_type, target_opset={'':15, 'ai.onnx.ml':3})
     
-    
-    with open("isolation_forest.onnx", "wb") as f:
-        f.write(onx.SerializeToString())
+        onnx_path=f"models/model_{binary_name}.onnx"
+        with open(onnx_path, "wb") as f:
+            f.write(onx.SerializeToString())
         
         
